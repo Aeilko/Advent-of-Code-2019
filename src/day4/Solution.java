f95ec2153cd711e56cfc26c1a4262ad149037ad8
Aeilko Bos <aeilkobos@hotmail.com>
2019-12-04 17:58:22 +0100

    Day 4 part 2.

--- src/day4/Solution.java
+++ src/day4/Solution.java
@@ -8,18 +8,9 @@ public class Solution {
 
 	public static final String INPUT = "138307-654504";
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-
-		String input = in.nextLine();
-		if(input.equals("def"))
-			input = INPUT;
-
-		String[] values = input.split("-");
-		int start = Integer.parseInt(values[0]);
-		int end = Integer.parseInt(values[1]);
-
+	public static int part1(int start, int end){
 		int result = 0;
+
 		// Way better methods to do this using some logic, but this is easier
 		outer: for(int i = start; i <= end; i++){
 			String val = Integer.toString(i);
@@ -38,9 +29,66 @@ public class Solution {
 				result++;
 		}
 
+		return result;
+	}
+
+	public static int part2(int start, int end){
+		int result = 0;
+
+		// Way better methods to do this using some logic, but this is easier
+		outer: for(int i = start; i <= end; i++){
+			String val = Integer.toString(i);
+
+			boolean hasDouble = false;
+			int hasDoubleDigit = 0;
+
+			int prev = 0;
+			int prev2 = 0;
+
+			for(char c: val.toCharArray()){
+				int cur = Character.getNumericValue(c);
+				if(cur < prev)
+					continue outer;
+				if(cur == prev) {
+					if(!hasDouble && prev != prev2){
+						hasDouble = true;
+						hasDoubleDigit = cur;
+					}
+					else if(prev == prev2 && cur == hasDoubleDigit) {
+						hasDouble = false;
+						hasDoubleDigit = 0;
+					}
+				}
+				prev2 = prev;
+				prev = cur;
+			}
+
+			if(hasDouble)
+				result++;
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+		String input = in.nextLine();
+		if(input.equals("def"))
+			input = INPUT;
+
+		String[] values = input.split("-");
+		int start = Integer.parseInt(values[0]);
+		int end = Integer.parseInt(values[1]);
+
+		int result = part1(start, end);
+
 		System.out.println("Part 1 - Success");
+		System.out.println("Answer\t" + result + "\n");
+
+		result = part2(start, end);
+
+		System.out.println("Part 2 - Success");
 		System.out.println("Answer\t" + result);
-
-
 	}
 }
